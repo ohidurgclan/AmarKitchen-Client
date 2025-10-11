2b4bd210adfdc9f2a5a7a2862f141fef747be8c8
ohidurgclan <ohidur.ru.2019@gmail.com>
2025-10-12 01:29:23 +0600

    Initial Commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            We are Cloud Kitchen Platform Name Amar Kitchen
+        </div>
+    );
+};
+
+export default AboutPage;
---
+++ src/app/contact/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactPage = () => {
+    return (
+        <div>
+            Go To hell
+        </div>
+    );
+};
+
+export default ContactPage;
