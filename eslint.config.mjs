63086888ec52639db1ebbfda1962449d3c4228e4
ohidurgclan <ohidur.ru.2019@gmail.com>
2025-11-23 02:17:50 +0600

    NextJs 16 Update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -21,5 +22,21 @@ const eslintConfig = [
     ],
   },
 ];
+=======
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
+>>>>>>> 9336571 (NextJs Version 16 Update)
 
 export default eslintConfig;
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,11 @@
 const config = {
+<<<<<<< HEAD
   plugins: ["@tailwindcss/postcss"],
+=======
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+>>>>>>> 9336571 (NextJs Version 16 Update)
 };
 
 export default config;
