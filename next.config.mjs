453a7665f3d595140bca836cba47a3d293225d88
ohidurgclan <ohidur.ru.2019@gmail.com>
2025-11-23 02:48:59 +0600

    NextJs 16 Update

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< HEAD
-const nextConfig = {};
-=======
 const nextConfig = {
   /* config options here */
 };
->>>>>>> 9336571 (NextJs Version 16 Update)
 
 export default nextConfig;
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,7 @@
 const config = {
-<<<<<<< HEAD
-  plugins: ["@tailwindcss/postcss"],
-=======
   plugins: {
     "@tailwindcss/postcss": {},
   },
->>>>>>> 9336571 (NextJs Version 16 Update)
 };
 
 export default config;
